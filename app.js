fdcb808e1fa718ca705381ed98c6741b9ae22e23
Wei <awei1127@hotmail.com>
2023-04-07 20:25:10 +0800

    Routing setup completed

--- app.js
+++ app.js
@@ -8,3 +8,22 @@ const port = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(express.static('public'))
+
+// 設定路由
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/restaurants/:id', (req, res) => {
+  res.render('show')
+})
+
+app.get('/search', (req, res) => {
+  res.render('index')
+})
+
+// 啟動並監聽
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`)
+})
