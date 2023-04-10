45d9a6922dc03a11c069c215b1728d62f74adda1
Wei <awei1127@hotmail.com>
2023-04-11 00:51:13 +0800

    refactor: add method-override

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const Restaurant = require('./models/restaurant')
 const exphbs = require('express-handlebars')
 const port = 3000
 const mongoose = require('mongoose') // 載入 mongoose
-const restaurant = require('./models/restaurant')
+const methodOverride = require('method-override')
 
 // 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -19,7 +19,7 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 // 使用body-parser(以在接下來能夠解析使用者送出的post request body)
 app.use(express.urlencoded({ extended: true }))
-
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
 
 // 設定路由-首頁
@@ -65,7 +65,7 @@ app.post('/restaurant', (req, res) => {
 })
 
 // 設定路由-送出刪除請求
-app.post('/restaurant/:id/delete', (req, res) => {
+app.delete('/restaurant/:id', (req, res) => {
   // 用request params來拿出使用者要刪除的資料id
   const id = req.params.id
   // 用Restaurant.findById()來找到該筆資料
@@ -86,25 +86,16 @@ app.get('/restaurant/:id/edit', (req, res) => {
 })
 
 // 設定路由-送出編輯內容
-app.post('/restaurant/:id/save', (req, res) => {
+app.put('/restaurant/:id', (req, res) => {
   // 用params拿出正在編輯的該筆資料的id
   const id = req.params.id
   // 用req.body拿出使用者輸入的修改後資料
   const data = req.body
   // 用Restaurant.findById()來找到該筆資料
   return Restaurant.findById(id)
-    // 把使用者輸入的資料賦值給這筆找到的資料
     .then(restaurant => {
-      // 暫時先這樣寫，待優化
-      restaurant.name = data.name
-      restaurant.name_en = data.name_en
-      restaurant.category = data.category
-      restaurant.image = data.image
-      restaurant.location = data.location
-      restaurant.phone = data.phone
-      restaurant.google_map = data.google_map
-      restaurant.rating = data.rating
-      restaurant.description = data.description
+      // 把使用者輸入的資料賦值給這筆找到的資料
+      Object.assign(restaurant, data)
       // 用這筆資料.save()來把資料儲存到資料庫
       return restaurant.save()
     })
