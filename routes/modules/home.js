b84bba3e14399c5cf077febe1c625613dfdd17fe
Wei <awei1127@hotmail.com>
2023-05-03 01:17:33 +0800

    feat: add relations between User and Restaurant models. add banner

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,20 +4,22 @@ const Restaurant = require('../../models/restaurant')
 
 // 設定路由-首頁
 router.get('/', (req, res) => {
-  return Restaurant.find()
+  const userId = req.user._id
+  return Restaurant.find({ userId })
     .lean()
     .then(restaurants => res.render('index', { restaurants }))
-    .catch(error => console.log(error))
+    .catch(error => console.error(error))
 })
 
 // 設定路由-搜尋頁
 router.get('/search', (req, res) => {
   const keyword = req.query.keyword
+  const userId = req.user._id
   // 回傳一個陣列 陣列內容為餐廳名符合搜尋字串的所有餐廳
-  return Restaurant.find({ name: new RegExp(keyword, 'i') })
+  return Restaurant.find({ userId, name: new RegExp(keyword, 'i') })
     .lean()
     .then(restaurants => res.render('index', { restaurants }))
-    .catch(error => console.log(error))
+    .catch(error => console.error(error))
 })
 
 // 設定路由-新增餐廳頁面
--- routes/modules/restaurant.js
+++ routes/modules/restaurant.js
@@ -4,52 +4,59 @@ const Restaurant = require('../../models/restaurant')
 
 // 設定路由-餐廳詳細頁
 router.get('/:id', (req, res) => {
-  return Restaurant.findById(req.params.id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ userId, _id })
     .lean()
     .then(restaurant => res.render('show', { restaurant }))
-    .catch(error => console.log(error))
+    .catch(error => console.error(error))
 })
 
 // 設定路由-送出新增資料
 router.post('/', (req, res) => {
+  const userId = req.user._id
   // 用request body來拿出使用者輸入的資料
-  const data = req.body
+  const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
   // 用Restaurant.create來把資料存到資料庫
-  return Restaurant.create(data)
+  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description, userId })
     // 重新導向一覽頁面
     .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
+    .catch(error => console.error(error))
 })
 
 // 設定路由-送出刪除請求
 router.delete('/:id', (req, res) => {
+  const userId = req.user._id
   // 用request params來拿出使用者要刪除的資料id
-  const id = req.params.id
-  // 用Restaurant.findById()來找到該筆資料
-  return Restaurant.findById(id)
+  const _id = req.params.id
+  // 找到該筆資料
+  return Restaurant.findOne({ userId, _id })
     // 用該筆資料.remove()方法來從資料庫移除該筆資料
     .then(restaurant => restaurant.remove())
     // 重新導向一覽頁面
     .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
+    .catch(error => console.error(error))
 })
 
 // 設定路由-編輯餐廳頁面
 router.get('/:id/edit', (req, res) => {
-  return Restaurant.findById(req.params.id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ userId, _id })
     .lean()
     .then(restaurant => res.render('edit', { restaurant }))
-    .catch(error => console.log(error))
+    .catch(error => console.error(error))
 })
 
 // 設定路由-送出編輯內容
 router.put('/:id', (req, res) => {
+  const userId = req.user._id
   // 用params拿出正在編輯的該筆資料的id
-  const id = req.params.id
+  const _id = req.params.id
   // 用req.body拿出使用者輸入的修改後資料
   const data = req.body
-  // 用Restaurant.findById()來找到該筆資料
-  return Restaurant.findById(id)
+  // 找到該筆資料
+  return Restaurant.findOne({ userId, _id })
     .then(restaurant => {
       // 把使用者輸入的資料賦值給這筆找到的資料
       Object.assign(restaurant, data)
@@ -57,8 +64,8 @@ router.put('/:id', (req, res) => {
       return restaurant.save()
     })
     // 重新導向詳細頁面
-    .then(() => res.redirect(`/restaurant/${id}`))
-    .catch(error => console.log(error))
+    .then(() => res.redirect(`/restaurant/${_id}`))
+    .catch(error => console.error(error))
 })
 
 module.exports = router
