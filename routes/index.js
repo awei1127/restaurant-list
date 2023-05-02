56a50d75514d24f32c25c31d9db0b25b4c23c6b0
Wei <awei1127@hotmail.com>
2023-05-03 00:04:58 +0800

    feat: add user authentication in restaurant and home routers

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
 // 引用 Express 與 Express 路由器
 const express = require('express')
 const router = express.Router()
+const { authenticator } = require('../middleware/auth')
 
 // 引入路由器
 const home = require('./modules/home')
 const restaurant = require('./modules/restaurant')
 const users = require('./modules/users')
 
-router.use('/', home)
-router.use('/restaurant', restaurant)
 router.use('/users', users)
+router.use('/restaurant', authenticator, restaurant)
+router.use('/', authenticator, home)
 
 // 匯出路由器
 module.exports = router
