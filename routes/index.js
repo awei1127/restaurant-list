04797a0baf77c3720381698985c0682621b7433a
Wei <awei1127@hotmail.com>
2023-05-02 18:07:20 +0800

    feat: add register feature

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,11 @@ const router = express.Router()
 // 引入路由器
 const home = require('./modules/home')
 const restaurant = require('./modules/restaurant')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/restaurant', restaurant)
+router.use('/users', users)
 
 // 匯出路由器
 module.exports = router
---
+++ routes/modules/users.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../../models/user')
+
+// 設定路由-登入頁面
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+// 設定路由-註冊頁面
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+// 設定路由-送出註冊表單
+router.post('/register', (req, res) => {
+
+  // 把註冊資料存入資料庫
+  const { name, email, password, confirmPassword } = req.body
+
+  // 看資料庫裡面是否已存在該email 存在則重新渲染 不存在則在資料庫建立使用者資料
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log('user exist')   // 預計要改成connect-flash訊息
+        return res.render('register')   // 預計要把使用者輸入的資料傳回前端渲染
+      }
+      User.create({ name, email, password })
+        .then(res.redirect('/users/login'))   // 註冊成功 導到登入頁面
+        .catch(err => console.error(err))
+    })
+    .catch(err => console.error(err))
+})
+
+// 匯出路由模組
+module.exports = router
