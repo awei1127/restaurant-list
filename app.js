ef378e82578bfe4fcdd17f2f88b81e0a7301020e
Wei <awei1127@hotmail.com>
2023-05-04 05:38:30 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const port = 3000
 const methodOverride = require('method-override')
 const routes = require('./routes')
 const usePassport = require('./config/passport')
+const flash = require('connect-flash')
 require('./config/mongoose')
 
 // setting template engine
@@ -23,6 +24,14 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.static('public'))
 usePassport(app)
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
 app.use(routes)
 
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -17,6 +17,7 @@ router.post('/login', passport.authenticate('local', {
 // 設定路由-登出
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
 
@@ -31,12 +32,30 @@ router.post('/register', (req, res) => {
   // 把註冊資料存入資料庫
   const { name, email, password, confirmPassword } = req.body
 
+  // 裝錯誤訊息的陣列
+  const errors = []
+
+  // 是否有空欄
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+
+  // 密碼與確認密碼是否一致
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符。' })
+  }
+
+  // 如果陣列內有錯誤訊息
+  if (errors.length) {
+    return res.render('register', { errors, name, email, password, confirmPassword })
+  }
+
   // 看資料庫裡面是否已存在該email 存在則重新渲染 不存在則在資料庫建立使用者資料
   User.findOne({ email })
     .then(user => {
       if (user) {
-        console.log('user exist')   // 預計要改成connect-flash訊息
-        return res.render('register')   // 預計要把使用者輸入的資料傳回前端渲染
+        errors.push({ message: '這個 Email 已經註冊過了。' })
+        return res.render('register', { errors, name, email, password, confirmPassword })
       }
       User.create({ name, email, password })
         .then(res.redirect('/users/login'))   // 註冊成功 導到登入頁面
